3eccb210919aed2fee087dca08cf3a3cd56d79f7
Askarov2001 <190103325@stu.sdu.edu.kz>
2021-03-13 22:11:44 +0600

    create posts index page

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\DB;
 
 use App\Models\Post;
 
+use App\Http\Controllers\PostController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -28,16 +30,13 @@ Route::get('/home', function () {
     return "home";
 });
 
-Route::get('/post/create', function () {
+Route::get('post/create', function () {
     DB::table('posts')->insert([
-        'title' => 'UFC',
-        'body' => 'McGregor'
+        'title' => 'Adlet',
+        'body' => 'Askarov'
     ]);
 });
 
-Route::get('post', function() {
-    $post = Post::find(1);
-    return $post-> body;
-});
+Route::get('post',[PostController::class, 'index']);
 
 
